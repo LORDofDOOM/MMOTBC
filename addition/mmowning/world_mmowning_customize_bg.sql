ec3e043617f8df9823e7c5ecea1be5b4699c2454
LORDofDOOM <noemail@nodomain.xyz>
2011-06-21 11:39:30 +0200

    Add SQLs
    Signed-off-by: LORDofDOOM <noemail@nodomain.xyz>

---
+++ addition/mmowning/world_mmowning_customize_bg.sql
@@ -0,0 +1,8 @@
+-- Alterac Valley Original 20
+UPDATE battleground_template SET MinPlayersPerTeam 	 = '10' WHERE id = 1;
+-- Warsong Gulch Original 5
+UPDATE battleground_template SET MinPlayersPerTeam 	 = '3' WHERE id = 2;
+-- Arathi Basin Original 8
+UPDATE battleground_template SET MinPlayersPerTeam 	 = '4' WHERE id = 3;
+-- Eye of The Storm Original 8
+UPDATE battleground_template SET MinPlayersPerTeam 	 = '4' WHERE id = 7;
---
+++ addition/mmowning/world_mmowning_version.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `version`;
+
+CREATE TABLE IF NOT EXISTS `version` (
+  `core_version` varchar(120) DEFAULT NULL COMMENT 'Core revision dumped at startup.',
+  `core_revision` bigint(20) unsigned DEFAULT NULL,
+  `db_version` varchar(120) DEFAULT NULL COMMENT 'Version of world DB.',
+  `script_version` varchar(120) DEFAULT NULL COMMENT 'Version of scripts DB.'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Version Notes';
+
+
+REPLACE INTO `version` (`core_version`, `core_revision`, `db_version`, `script_version`) VALUES
+('MMOwningCore Rev: 0 Release Hash: (Unix,little-endian)', 0, 'MMODB base ODB 0.0.7 DEV  for OregonCore rev.1391', 'ACID 2.0.4 - Full Release for Oregon Core (2.4.3 Client)');
